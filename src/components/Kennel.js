55590b46e190102b921fdb3ae88da91890e7fb00
Daniel Brewer <danielbrewer2014@comcast.net>
2019-01-17 15:46:46 -0600

    established ownership relationships, console logging correctly

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import AnimalList from "./animal/AnimalList"
 import EmployeeList from "./employee/EmployeeList"
 import LocationList from "./location/LocationList"
+import OwnerList from "./owner/OwnerList"
 import "./Kennel.css"
 
 
@@ -26,17 +27,36 @@ class Kennel extends Component {
         { id: 2, name: "Nashville South", address: "10101 Binary Court" }
     ]
     animalsFromAPI = [
-        { id: 1, name: "Cinnamon", breed: "Boxer" },
-        { id: 2, name: "Lily", breed: "Labrador" },
-        { id: 3, name: "Snowball", breed: "Cat" },
-        { id: 4, name: "Finn", breed: "Labrador" },
-        { id: 5, name: "Izzy", breed: "Pomeranian" }
+        { id: 1, ownerId: 5, name: "Cinnamon", breed: "Boxer" },
+        { id: 2, ownerId: 4, name: "Lily", breed: "Labrador" },
+        { id: 3, ownerId: 3, name: "Snowball", breed: "Cat" },
+        { id: 4, ownerId: 2, name: "Finn", breed: "Labrador" },
+        { id: 5, ownerId: 1, name: "Izzy", breed: "Pomeranian" }
     ]
 
+    ownersFromAPI = [
+        { id: 1, name: "Ryan Tanay" },
+        { id: 2, name: "Emma Beaton" },
+        { id: 3, name: "Dani Adkins" },
+        { id: 4, name: "Adam Oswalt" },
+        { id: 5, name: "Fletcher Bangs" },
+        { id: 6, name: "Angela Lee" }
+    ]
+    ownershipsFromAPI = [
+        { id: 1, ownerId: 5 , animalId: 1},
+        { id: 2, ownerId: 6 , animalId: 1},
+        { id: 3, ownerId: 4 , animalId: 3},
+        { id: 4, ownerId: 3 , animalId: 5},
+        { id: 5, ownerId: 2, animalId: 2},
+        { id: 6, ownerId: 1 , animalId: 4}
+    ]
+    
     state = {
+        owners: this.ownersFromAPI,
         employees: this.employeesFromAPI,
         locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI
+        animals: this.animalsFromAPI,
+        ownerships: this.ownershipsFromAPI
     }
 
     render() {
@@ -44,7 +64,8 @@ class Kennel extends Component {
             <article className="kennel">
                 <LocationList locations={this.state.locations} />
                 <EmployeeList employees={this.state.employees} />
-                <AnimalList animals={this.state.animals} />
+                <AnimalList animals={this.state.animals} owners ={this.state.owners} />
+                <OwnerList owners={this.state.owners} />
             </article>
         )
     }
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
-
 class AnimalList extends Component {
     render() {
         return (
             <section className="animals">
-            {
-                this.props.animals.map(animal =>
-                    <div key={animal.id}>
-                        {animal.name}
-                    </div>
-                )
-            }
+                {
+                    this.props.animals.forEach(animal => {
+                        this.props.owners.forEach(owner => {
+                            if (animal.ownerId === owner.id) {
+                                console.log("animal", animal)
+                                console.log("owner", owner)
+                    return (
+                                    <div>
+                                        <div key={animal.id}>
+                                            {animal.name}
+                                        </div>
+                                        <h6>owned by</h6>
+                                        <div key={owner.id}>
+                                            {owner.name}
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })
+                    }
+                    )
+                }
             </section>
         )
     }
