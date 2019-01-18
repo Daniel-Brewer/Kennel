a89bc171bc0503f7f988399e1e7e159170cf82f3
Daniel Brewer <danielbrewer2014@comcast.net>
2019-01-18 13:39:53 -0600

    corrected kennel.json

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,43 +6,32 @@ import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
 
 
-class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, name: "Ryan Tanay", phoneNumber: "615-123-45678" },
-        { id: 2, name: "Emma Beaton", phoneNumber: "615-456-7891" },
-        { id: 3, name: "Dani Adkins", phoneNumber: "615-789-1234" },
-        { id: 4, name: "Adam Oswalt", phoneNumber: "615-012-3456" },
-        { id: 5, name: "Fletcher Bangs", phoneNumber: "615-234-5678" },
-        { id: 6, name: "Angela Lee", phoneNumber: "615-012-3456" }
-    ]
-
+export default class ApplicationViews extends Component {
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI
+        animals: [],
+        employees: [],
+        locations: [],
+        owners: []
     }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/locations")
+            .then(r => r.json())
+            .then(locations => newState.locations = locations)
+            .then(() => fetch("http://localhost:5002/animals")
+            .then(r => r.json()))
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/owners")
+            .then(r => r.json()))
+            .then(owners => newState.owners = owners)
+            .then(() => this.setState(newState))
+    }
+
 
     render() {
         return (
@@ -50,18 +39,16 @@ class ApplicationViews extends Component {
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
-                <Route path="/animals" render={(props) => {
+                <Route exact path="/animals" render={(props) => {
                     return <AnimalList animals={this.state.animals} />
                 }} />
-                <Route path="/employees" render={(props) => {
+                <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
-                <Route path="/owners" render={(props) => {
+                <Route exact path="/owners" render={(props) => {
                     return <OwnerList owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
     }
 }
-
-export default ApplicationViews
