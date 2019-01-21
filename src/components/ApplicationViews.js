6efcd4123d34faf7ec4ebc36b4de52f32de9210a
Daniel Brewer <danielbrewer2014@comcast.net>
2019-01-21 15:56:57 -0600

    employee icon and list working now

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -12,6 +12,44 @@ export default class ApplicationViews extends Component {
         employees: [],
         locations: [],
         owners: []
+    };
+
+    deleteAnimal = id => {
+        return fetch(`http://localhost:5002/animals/${id}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+            .then(() => fetch(`http://localhost:5002/animals`))
+            .then(response => response.json())
+            .then(animals => this.setState({
+                animals: animals
+            })
+            )
+    }
+    deleteEmployee = id => {
+        return fetch(`http://localhost:5002/employees/${id}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+            .then(() => fetch(`http://localhost:5002/employees`))
+            .then(response => response.json())
+            .then(employees => this.setState({
+                employees: employees
+            })
+            )
+    }
+    
+    deleteOwner = id => {
+        return fetch(`http://localhost:5002/owners/${id}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+            .then(() => fetch(`http://localhost:5002/owners`))
+            .then(response => response.json())
+            .then(owners => this.setState({
+                owners: owners
+            })
+            )
     }
 
     componentDidMount() {
@@ -21,32 +59,33 @@ export default class ApplicationViews extends Component {
             .then(r => r.json())
             .then(locations => newState.locations = locations)
             .then(() => fetch("http://localhost:5002/animals")
-            .then(r => r.json()))
+                .then(r => r.json()))
             .then(animals => newState.animals = animals)
             .then(() => fetch("http://localhost:5002/employees")
-            .then(r => r.json()))
+                .then(r => r.json()))
             .then(employees => newState.employees = employees)
             .then(() => fetch("http://localhost:5002/owners")
-            .then(r => r.json()))
+                .then(r => r.json()))
             .then(owners => newState.owners = owners)
             .then(() => this.setState(newState))
     }
+
 
 
     render() {
         return (
             <React.Fragment>
                 <Route exact path="/" render={(props) => {
-                    return <LocationList locations={this.state.locations} />
+                    return <LocationList deleteLocation={this.deleteLocation} locations={this.state.locations} />
                 }} />
                 <Route exact path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                    return <AnimalList deleteAnimal={this.deleteAnimal} animals={this.state.animals} />
                 }} />
                 <Route exact path="/employees" render={(props) => {
-                    return <EmployeeList employees={this.state.employees} />
+                    return <EmployeeList deleteEmployee={this.deleteEmployee} employees={this.state.employees} />
                 }} />
                 <Route exact path="/owners" render={(props) => {
-                    return <OwnerList owners={this.state.owners} />
+                    return <OwnerList deleteOwner={this.deleteOwner} owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
